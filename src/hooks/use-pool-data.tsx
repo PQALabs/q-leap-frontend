9d97141ee78173f3dbebdc57759ca7badb33ded2
phucvarmeta <phucth@var-meta.com>
2026-05-05 11:49:17 +0700

    chore: remove console.log

--- src/hooks/use-pool-data.tsx
+++ src/hooks/use-pool-data.tsx
@@ -68,7 +68,6 @@ export function usePoolData(
   let reserves: ReservesDataHumanized | undefined = undefined;
   if (reservesDataRaw) {
     const [rawReserves, rawBaseCurrency] = reservesDataRaw;
-    console.log('🚀 ~ usePoolData ~ rawBaseCurrency:', rawBaseCurrency);
 
     const reservesData: ReserveDataHumanized[] = rawReserves.map((r) => ({
       id: `${lendingPoolAddressProvider}-${r.underlyingAsset}`.toLowerCase(),
@@ -113,7 +112,6 @@ export function usePoolData(
       networkBaseTokenPriceInUsd: rawBaseCurrency.networkBaseTokenPriceInUsd.toString(),
       networkBaseTokenPriceDecimals: rawBaseCurrency.networkBaseTokenPriceDecimals,
     };
-    console.log('🚀 ~ usePoolData ~ baseCurrencyData:', baseCurrencyData);
 
     // We get unit instead of decimals in V2. Let's calculate decimals based on unit.
     baseCurrencyData.marketReferenceCurrencyDecimals =
