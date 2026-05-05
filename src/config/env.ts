173c6b5e9fda7a7a5562b7b689bc8b46319fdb41
phucvarmeta <phucth@var-meta.com>
2026-05-05 15:36:57 +0700

    chore: add debug logs for api environment variables in configuration and liquidation module

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,5 @@
+console.log('🚀 ~ process.env.NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+
 export const env = {
   APP_NAME: process.env.NEXT_PUBLIC_APP_NAME ?? 'Q-LEAP',
   APP_URL: process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000',
